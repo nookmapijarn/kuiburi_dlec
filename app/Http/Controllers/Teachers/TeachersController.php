8491a6687a144c0d51eaa5da7301d84fdebd3702
nookmapijarn <nookmapijarn@gmail.com>
2023-08-11 15:06:08 +0700

    first

---
+++ app/Http/Controllers/Teachers/TeachersController.php
@@ -0,0 +1,241 @@
+<?php
+
+namespace App\Http\Controllers\Teachers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+
+class TeachersController extends Controller
+{
+    protected $semestry = '66/1';
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index(Request $request)
+    {
+        //
+        $data=[];
+        $tumbon = '';
+        $studreport = '';
+        $semestry = $this->semestry;
+        $id = auth()->user()->student_id;
+
+        if ($id != '1215040001') {
+            return redirect('welcome/?roletype='.$id);
+        }
+
+        if($request->tumbon!=''){
+            $tumbon = str_split($request->tumbon, 4)[0];
+            $studreport = $request->studreport;
+        }else{
+            return view('teachers.tdashboard' ,compact('data', 'semestry'));
+        }
+
+        // เลือกรายงาน
+        switch ($studreport) {
+            case 'นักศึกษาทั้งหมด':
+                $data = $this->allstudent($tumbon);
+                $data = collect($data)->sortBy('lavel')->toArray(); // $mystudent = collect($mystudent)->sortBy('lavel')->reverse()->toArray(); DESC
+                return view('teachers.tdashboard' ,compact('data', 'semestry'));
+              break;
+            case 'เฉพาะผู้คาดว่าจะจบ':
+                $data = $this->expstudent($tumbon, $studreport);
+                $data = collect($data)->sortBy('lavel')->toArray();
+                return view('teachers.tdashboard' ,compact('data', 'semestry'));
+              break;
+            case 'ไม่จบตกค้าง(ที่ไม่ได้ลงทะเบียนแล้ว)':
+                $data = $this->unfinishstudent($tumbon, $studreport);
+                $data = collect($data)->sortBy('lavel')->toArray();
+                return view('teachers.tdashboard' ,compact('data', 'semestry'));
+              break;
+            default:
+              return view('teachers.tdashboard' ,compact('data', 'semestry'));
+          }                       
+        //return view('teachers.tdashboard' ,compact('data'));
+    }
+
+    public function allstudent($grp_code){
+        $current_student = $this->current_student($grp_code);
+        $allstudent = [];
+
+        foreach ($current_student as $g) {
+            $student = $this->get_student($g->STD_CODE);
+            foreach ($student as $s){
+
+                switch ($this->expfin($s->ID)) {
+                    case true :
+                      $expfin = 1;
+                      $nnet = ($s->NT_SEM!='' ? 'ผ่านแล้ว' : ($s->NT_NOSEM!='' ? 'E-Exam': 'มีสิทธิ'));
+                      break;
+                    case false:
+                      $expfin = 0;
+                      $nnet = ($s->NT_SEM!='' ? $s->NT_SEM : ($s->NT_NOSEM!='' ? 'E-Exam': '-'));
+                      break;
+                    default:
+                        $expfin = '*';
+                        $nnet = '*';
+                  }
+
+                array_push($allstudent, 
+                    [
+                        'id'        =>  $s->ID,
+                        'lavel'     =>  $this->lavelis($s->STD_CODE),  
+                        'name'      =>  $s->NAME,
+                        'surname'   =>  $s->SURNAME,
+                        'expfin'    =>  $expfin,
+                        'activity'  =>  $this->get_activity($s->STD_CODE),
+                        'nt_sem'    =>  $nnet
+
+                    ]
+                );
+            }
+        }
+        
+        return $allstudent;        
+    }
+
+    public function expstudent($grp_code){
+        $current_student = $this->current_student($grp_code);
+        $expstudent = [];
+
+        foreach ($current_student as $g) {
+            $student = $this->get_student($g->STD_CODE);
+            foreach ($student as $s){
+
+                switch ($this->expfin($s->ID)) {
+                    case true :
+                      $expfin = 1;
+                      $nnet = ($s->NT_SEM!='' ? 'ผ่านแล้ว' : ($s->NT_NOSEM!='' ? 'E-Exam': 'มีสิทธิ'));
+                      array_push($expstudent, 
+                        [
+                            'id'        =>  $s->ID,
+                            'lavel'     =>  $this->lavelis($s->STD_CODE),  
+                            'name'      =>  $s->NAME,
+                            'surname'   =>  $s->SURNAME,
+                            'expfin'    =>  $expfin,
+                            'activity'  =>  $this->get_activity($s->STD_CODE),
+                            'nt_sem'    =>  $nnet
+    
+                        ]
+                      );
+                      break;
+                    case false:
+                      break;
+                    default:
+                        $expfin = '*';
+                        $nnet = '*';
+                  }
+            }
+        }
+        
+        return $expstudent;        
+    }
+
+    public function unfinishstudent($grp_code){
+        $current_student = $this->all_student($grp_code);
+        $expstudent = [];
+
+        foreach ($current_student as $g) {
+            $student = $this->get_student($g->STD_CODE);
+            foreach ($student as $s){
+
+                switch ($this->exp_unfin($s->ID)) {
+                    case true :
+                      $expfin = 1;
+                      $nnet = ($s->NT_SEM!='' ? 'ผ่านแล้ว' : ($s->NT_NOSEM!='' ? 'E-Exam': 'มีสิทธิ'));
+                      array_push($expstudent, 
+                        [
+                            'id'        =>  $s->ID,
+                            'lavel'     =>  $this->lavelis($s->STD_CODE),  
+                            'name'      =>  $s->NAME,
+                            'surname'   =>  $s->SURNAME,
+                            'expfin'    =>  $expfin,
+                            'activity'  =>  $this->get_activity($s->STD_CODE),
+                            'nt_sem'    =>  $nnet
+    
+                        ]
+                      );
+                      break;
+                    case false:
+                      break;
+                    default:
+                        $expfin = '*';
+                        $nnet = '*';
+                  }
+            }
+        }
+        
+        return $expstudent;        
+    }
+    // ข้อมูลนักศึกษา
+    public function get_student($std_code){
+        $student = DB::table('student')
+        ->where('STD_CODE', $std_code)
+        ->select(array('ID', 'STD_CODE', 'NAME', 'SURNAME', 'NT_SEM', 'NT_NOSEM'))
+        ->get();
+        return $student;
+    }
+
+    // ตารางคาดว่าจะจบ
+    public function expfin($student_id){
+        $expfin = DB::table('expectfin')->where('ID', $student_id)->first();
+        if($expfin === null){
+            return false;
+        } else {
+            return true;
+        }       
+    }
+
+    // ตารางไม่จบตกค้าง
+    public function exp_unfin($student_id){
+        $expfin = DB::table('expectfin1')->where('ID', $student_id)->first();
+        if($expfin === null){
+            return false;
+        } else {
+            return true;
+        }       
+    }
+    public function current_student($grp_code){
+        // ตาราง garde
+        $grade = DB::table('grade')
+        ->where('GRP_CODE', $grp_code)
+        ->where('SEMESTRY', $this->semestry)
+        ->select('STD_CODE')
+        ->orderBy('STD_CODE', 'ASC')
+        ->groupBy('STD_CODE')
+        ->get();
+        
+        return $grade;
+    }
+
+    public function all_student($grp_code){
+        // ตาราง garde
+        $student = DB::table('student')
+        ->where('GRP_CODE', $grp_code)
+        ->select('STD_CODE')
+        ->orderBy('STD_CODE', 'ASC')
+        ->groupBy('STD_CODE')
+        ->get();
+        
+        return $student;
+    }
+    public function get_activity($std_code){
+        $activity = DB::table('activity')
+        ->where('STD_CODE', $std_code)
+        ->sum('HOUR');
+        return $activity;
+    }
+    public function lavelis($student_id){
+        // ดูระดับชั้น
+        $str = $student_id;
+        $split = str_split($str, 1);
+        return $split[13];
+    }
+
+}
---
+++ resources/views/teachers/tdashboard.blade.php
@@ -0,0 +1,127 @@
+<x-teachers-layout>
+    <x-slot name="header">
+        <h6 class="font-semibold text-lg text-gray-800 dark:text-gray-200 leading-tight m-2">
+            {{ __('ภาคเรียนปัจจุบัน') }} {{$semestry}}
+        </h6>
+        <form method="GET" action="{{ route('tdashboard') }}" class="">
+        <div class="grid grid-cols-1 gap-2 md:grid md:grid-cols-2 justify-items-center">
+            <div class="min-w-full" >
+                {{-- <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ศสกร.ตำบล</label> --}}
+                <select required id="tumbon" name="tumbon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 ">
+                  {{-- @if(request()->get('tumbon')!='') <option selected>{{request()->get('tumbon')}}</option> @endif
+                  @if(request()->get('tumbon')=='') <option selected>เลือกตำบล</option> @endif --}}
+                  <option value="">เลือกตำบล</option>
+                  <option value="4011 บางพลับ">4011 บางพลับ</option>
+                  <option value="4012 บางพลับ">4012 บางพลับ</option>
+                  <option value="4021 อ่างแก้ว">4021 อ่างแก้ว</option>
+                  <option value="4031 หนองแม่ไก่">4031 หนองแม่ไก่</option>
+                  <option value="4041 ยางช้าย">4041 ยางช้าย</option>
+                  <option value="4051 โพธิ์รังนก">4051 โพธิ์รังนก</option>
+                  <option value="4061 รำมะสัก">4061 รำมะสัก</option>
+                  <option value="4071 บางระกำ">4071 บางระกำ</option>
+                  <option value="4081 บ่อแร่">4081 บ่อแร่</option>
+                  <option value="4091 สามง่าม">4091 สามง่าม</option>
+                  <option value="4101 ทางพระ">4101 ทางพระ</option>
+                  <option value="4102 ทางพระ">4102 ทางพระ</option>
+                  <option value="4111 อินทประมูล">4111 อินทประมูล</option>
+                  <option value="4121 องครักษ์">4121 องครักษ์</option>
+                  <option value="4131 โคกพุทรา">4131 โคกพุทรา</option>
+                  <option value="4141 บางเจ้าฉ่า">4141 บางเจ้าฉ่า</option>
+                  <option value="4151 คำหยาด">4151 คำหยาด</option>
+                  <option value="4171 พิการ">4171 พิการ</option>
+                </select>
+              </div>
+            <div class="min-w-full">
+                {{-- <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">รายงาน</label> --}}
+                <select required id="studreport" name="studreport" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  {{-- @if(request()->get('studreport')!='') <option selected>{{request()->get('studreport')}}</option> @endif
+                  @if(request()->get('studreport')=='') <option selected >เลือกรายงาน</option> @endif --}}
+                  <option value="">เลือกรายงาน</option>
+                  <option value="นักศึกษาทั้งหมด">นักศึกษาทั้งหมด</option>
+                  <option value="เฉพาะผู้คาดว่าจะจบ">เฉพาะผู้คาดว่าจะจบ</option>
+                  <option value="ไม่จบตกค้าง(ที่ไม่ได้ลงทะเบียนแล้ว)">ไม่จบตกค้าง (ที่ไม่ได้ลงทะเบียนแล้ว)</option>
+                </select>
+            </div>
+          </div>
+          <button type="submit" class="rounded-full p-2 mt-2 min-w-full bg-indigo-500 text-white">ดูรายงาน</button> 
+        </form>
+        {{-- <div>
+          <span class="text-red-500 font-medium">หมายเหตุ</span>
+        </div> --}}
+        <div class="grid grid-cols-3 gap-2 mt-2">
+          <div class="font-semibold text-xs truncate"> <span class="text-indigo-500">N-NET</span> : มีสิทธิสอบ N-NET ภาคเรียนปัจจุบัน </div> 
+          <div class="font-semibold pl-2 text-xs truncate"> <span class="text-yellow-500">E-EXAM</span> : มีสิทธิสอบ E-EXAM ภาคเรียนปัจจุบัน </div>
+          <!-- Tag 1-->
+          <div class="flex items-center text-xs">
+            <div class="text-blue-100 mr-1">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 fill-yellow-500">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+              </svg>              
+            </div>
+            <span class="text-indigo font-semibold truncate"> : คาดว่าจะจบหลักสูตร</span>
+          </div>
+        </div>
+    </x-slot>
+
+    <div class="flex flex-col max-w-srceen-lg">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full  py-2 sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+                <table class="min-w-full text-left text-sm font-light">
+                <div class="flex flex-col-1 flex-row-1 justify-center p-4 bg-indigo-200 drop-shadow">
+                  <div class="text-sm font-bold">ตำบล : <span class="font-normal">{{request()->get('tumbon')}}</span></div>
+                  <div class="text-sm font-bold pl-4">รายงาน : <span class=" font-normal">{{request()->get('studreport')}}</span></div>
+                </div>
+                <thead class="border-b font-medium bg-white drop-shadow text-xs md:text-sm">                 
+                  <tr class="bg-gray-200">
+                    <th scope="col" class="p-2 text-center">ลำดับ</th>
+                    <th scope="col" class="p-2">รหัส</th>
+                    <th scope="col" class="p-2 hidden md:block">ระดับ</th>
+                    <th scope="col" class="p-2">ชื่อ</th>
+                    <th scope="col" class="p-2">นามสกุล</th>
+                    <th scope="col" class="p-2">คาดว่าจะจบ</th>
+                    <th scope="col" class="p-2 text-center">กพช.</th>
+                    <th scope="col" class="p-2 text-center">N-NET</th>
+                  </tr>
+                </thead>
+                <tbody class="text-xs md:text-sm">
+                @foreach($data as $d)
+                  <tr 
+                  @if($d['lavel']==1) class="border-b bg-pink-100 shadow-md hover:bg-pink-300" @endif
+                  @if($d['lavel']==2) class="border-b bg-green-100 shadow-md hover:bg-green-300" @endif
+                  @if($d['lavel']==3) class="border-b bg-yellow-100 shadow-md hover:bg-yellow-300" @endif
+                  >
+                    <td class="p-2 text-center">{{$loop->iteration}}</td>
+                    <td class="p-2">{{$d['id']}}</td>
+                    <td class="p-2 hidden md:block">{{$d['lavel']}}</td>
+                    <td class="p-2 w-15">{{$d['name']}}</td>
+                    <td class="p-2">{{$d['surname']}}</td>
+                    @if($d['expfin']==1)
+                    <td class="p-2 text-violet-500 items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 fill-yellow-500">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                      </svg>                                           
+                    </td>
+                    @else
+                    <td class="p-2">-</td>
+                    @endif
+                    <td class="p-2 text-center">{{$d['activity']}}</td>
+                    @if($d['nt_sem']=="ผ่านแล้ว")
+                    <td class="p-2 text-center text-green-500">{{$d['nt_sem']}}</td>
+                    @elseif($d['nt_sem']=="มีสิทธิ")
+                    <td class="p-2 text-center text-indigo-500">N-NET</td>
+                    @elseif($d['nt_sem']=="E-Exam")
+                    <td class="p-2 text-center text-yellow-500">{{$d['nt_sem']}}</td>
+                    @else
+                    <td class="p-2 text-center">{{$d['nt_sem']}}</td>
+                    @endif
+                  </tr>
+                @endforeach
+                </tbody>
+              </table>
+              @if($data==null) <section class="p-10 text-center text-lg">**ไม่พบข้อมูล กรุณาเลือกรายการใหม่**</section> @endif
+            </div>
+          </div>
+        </div>
+      </div>
+</x-teachers-layout>
